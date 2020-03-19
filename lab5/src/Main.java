ecae61ca77c509766e544c3c14e2399ed527510a
denis15yo <denis.shilovich@bk.ru>
2020-03-19 10:24:41 +0300

    javafx imported

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Title");
+
+        stage.show();
     }
 }
